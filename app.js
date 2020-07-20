7ce38047ae8008640c58375832e8b43735538bdf
tomseoyun <32997822+tomseoyun@users.noreply.github.com>
2020-07-20 23:28:48 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+import {Polygon} from './polygon.js';
+class App {
+	constructor(){
+		this.canvas = document.createElement('canvas');
+		document.body.appendChild(this.canvas);
+		this.ctx = this.canvas.getContext('2d');
+
+		this.pixelRatio=window.devicePixelRatio > 1 ? 2 : 1;
+
+		window.addEventListener('resize', this.resize.bind(this), false);
+		this.resize();
+
+		window.requestAnimationFrame(this.animate.bind(this));
+			}
+
+
+	resize(){
+		this.stageWidth=document.body.clientWidth;
+		this.stageHeight=document.body.clientHeight;
+
+		this.canvas.width=this.stageWidth * this.pixelRatio;
+		this.canvas.height = this.stageHeight * this.pixelRatio;
+		this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+		this.polygon = new Polygon(
+
+			this.stageWidth /2,
+			this.stageHeight /2,
+			this.stageHeight /3,
+			3
+		);
+
+	}
+
+	animate(){
+		window.requestAnimationFrame(this.animate.bind(this));
+	
+		this.ctx.clearRect(0,0, this.stageWidth,this.stageHeight);
+
+		this.polygon.animate(this.ctx);
+
+	}
+
+
+
+window.onload=() => {
+	new App();
+
+}
